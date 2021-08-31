0dddbca5708e07e9564af15056de5ac4c67aa810
YinTeh <79126239+YinTeh@users.noreply.github.com>
2021-08-31 16:38:34 +0800

    linked list added

--- testing/main.cpp
+++ testing/main.cpp
@@ -1,11 +1,53 @@
 #include<iostream>
+#include<array>
+#include<sstream>
+
 using namespace std;
+
+struct patient_info {
+	string id;
+	string f_name;
+	string l_name;
+	int age;
+	string gender;
+	string phone;
+	string address;
+};
+
+struct date_time {
+	int date;
+	int month;
+	int year;
+	int hour;
+	int minute;
+};
+
+struct waiting_node {
+	struct patient_info info;
+	string sickness_descp;
+	struct date_time datetime;
+	bool disab_option;
+	string doct_name;
+	string med_info;
+	struct waiting_node* prev;
+	struct waiting_node* next;
+};
+
+struct history_node {
+	struct patient_info info;
+	string sickness_descp;
+	struct date_time datetime;
+	bool disab_option;
+	string doct_name;
+	string med_info;
+	struct waiting_node* prev;
+	struct waiting_node* next;
+};
+
+struct waiting_node* wRoot = NULL;
+struct history_node* hRoot = NULL;
 
 int main()
 {
-	cout << "Hiiiiiii";
-	cout << "TEsing"; 
-	cout << "Testing";
-	cout << "Testin 2";
-	cout << "hello";
+
 }
