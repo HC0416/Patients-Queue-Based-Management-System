eddb96b95785140fbb46e1656e70816f97bfa33d
YinTeh <79126239+YinTeh@users.noreply.github.com>
2021-11-01 22:46:58 +0800

    fixed sorting function bug

--- testing/main.cpp
+++ testing/main.cpp
@@ -605,6 +605,10 @@ public:
 
     //this one for question 1.6, sort patients by visit time, (need to add into the main menu)
     void sortByTime() {
+        while(wHead == NULL){
+            cout << "No record to sort!"<<endl;
+            return;
+        }
         int length = findLength();
         Node* nodeArrPtr = convertToArray();
         quickSort(nodeArrPtr, 0, length - 1);
